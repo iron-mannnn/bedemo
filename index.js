75f8e002425c724711af041b45e968f6c05c4abf
iron-mannnn <152364859+iron-mannnn@users.noreply.github.com>
2025-04-23 11:56:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello Human Being");
+});
+
+app.get("/instagram", (req, res) => {
+  res.send("Human Instagram");
+});
+
+app.get("/car", (req, res) => {
+  res.send("<h1>Human Car - Gwagon</h1>");
+});
+
+app.get("/phone", (req, res) => {
+  res.send("<h2>Samsung S26</h2>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
